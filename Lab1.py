6c6e6bf0a4117d835e5225c9a8c42fa0c3540b54
Balint-Marian013 <265559984+Balint-Marian013@users.noreply.github.com>
2026-03-11 17:43:33 +0200

    „Was machst du, Bruder?”

---
+++ Lab1.py
@@ -0,0 +1,29 @@
+# #integer
+# print(10+3)
+# print(abs(-50))
+# print(bin(512))
+# print(10/3)
+# print(10%3)
+
+# Strings
+
+# # name="Marian"
+# print(name[2])
+# print(name[:])
+# print(name[1:])
+# print(name[:1])
+# print(name[-1])
+# print(name[::1])
+# 'Hi there' + 'Jonny'
+# print('*'*10)
+
+# # Boolean
+# #
+# # varsta = 47
+# # este_major = varsta >= 18
+# #
+# # if este_major == True:
+# #     print("fals")
+# # else:
+# #     print('adevarat')
+
