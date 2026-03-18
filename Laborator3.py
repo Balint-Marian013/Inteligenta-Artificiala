046e25b37282c39b12582edd25a96ce7fcb4ab2e
Balint-Marian013 <nicolaemarian.balint@ulbsibiu.ro>
2026-03-18 17:39:08 +0200

    lab3
    ..

---
+++ Laborator3.py
@@ -0,0 +1,80 @@
+
+# Liste
+# #my_list=[1,2,"3",True]
+# #print(len(my_list))
+# print(my_list.index("3"))
+# print(my_list.count(2))
+#
+# print(my_list[3])
+# print(my_list[1:])
+# print(my_list[:2])
+# print(my_list[-1])
+# print(my_list[::1])
+# print(my_list[::-1])
+# print(my_list[0:2:3])
+
+
+#Dictionary
+# my_dict = {'name': 'John Doe', 'age': 25, 'magic_power': False}
+#
+# print(my_dict['name'])
+# print(len(my_dict))
+# print(list(my_dict.keys()))
+# print(list(my_dict.values()))
+# print(list(my_dict.items()))
+#
+# my_dict['favourite_snack'] = 'Grapes'
+# print(my_dict)
+#
+# print(my_dict.get('age'))
+# print(my_dict.get('ages', 0))
+#
+# del my_dict['name']
+# print(my_dict)
+#
+# print(my_dict.pop('name', None))
+# print(my_dict)
+#Set
+# my_set = set()
+# my_set.add(1)
+# my_set.add(100)
+# my_set.add(100)
+#
+# new_list = [1,2,3,3,3,4,4,5,6,1]
+# print(set(new_list))
+#
+# my_set.remove(100)
+# my_set.discard(100)
+# my_set.clear()
+#
+# new_set = {1,2,3}.copy()
+# print(new_set)
+#
+# set1 = {1,2,3}
+# set2 = {3,4,5}
+#
+# set3 = set1.union(set2)
+# print(set3)
+#
+# set4 = set1.intersection(set2)
+# print(set4)
+#
+# set5 = set1.difference(set2)
+# print(set5)
+#
+# set6 = set1.symmetric_difference(set2)
+# print(set6)
+#
+# print(set1.issubset(set2))
+# print(set1.issuperset(set2))
+#
+
+#Tuple
+my_tuple=('apple','grapes','mango','grapes')
+apple,grapes,mango,grapes = my_tuple
+len(my_tuple)
+print(my_tuple[2])
+print(my_tuple[-1])
+
+type(None)
+a=None
